377c90215c27cfd78b5ba15fe7cc0c718f04363f
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-02 22:56:33 +0530

    comment a code for a while

--- variable.tf
+++ variable.tf
@@ -1,43 +1,43 @@
-variable "env"{}
-variable "instance_type"{}
-variable "vpc_cidr_block"{}
-variable "availability_zone" {}
-variable "frontend-subnets"{}
-variable "backend-subnets"{}
-variable "db-subnets"{}
-variable "public-subnets"{}
-variable "default_vpc_id" {}
-variable "default_vpc_cidr"{}
-variable "default_route-table_id"{}
-variable "bastion_nodes"{}
-variable "ssh_user"{}
-variable "ssh_pass"{}
-# variable "vault_token"{}
-# variable "engine"{}
-# variable "engine_version"{}
-# variable "allocated_storage"{}
-# variable "skip_final_snapshot"{}
-# variable "family"{}
-# variable "instance_class"{}
-# variable "storage_type"{}
-# variable "vpc_id"{}
-# variable "access_sg_app_port"{}
-# variable "kms_key_id"{}
-variable "eks-subnets"{}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# variable "env"{}
+# variable "instance_type"{}
+# variable "vpc_cidr_block"{}
+# variable "availability_zone" {}
+# variable "frontend-subnets"{}
+# variable "backend-subnets"{}
+# variable "db-subnets"{}
+# variable "public-subnets"{}
+# variable "default_vpc_id" {}
+# variable "default_vpc_cidr"{}
+# variable "default_route-table_id"{}
+# variable "bastion_nodes"{}
+# variable "ssh_user"{}
+# variable "ssh_pass"{}
+# # variable "vault_token"{}
+# # variable "engine"{}
+# # variable "engine_version"{}
+# # variable "allocated_storage"{}
+# # variable "skip_final_snapshot"{}
+# # variable "family"{}
+# # variable "instance_class"{}
+# # variable "storage_type"{}
+# # variable "vpc_id"{}
+# # variable "access_sg_app_port"{}
+# # variable "kms_key_id"{}
+# variable "eks-subnets"{}
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
