019c6bd89fecd18c27608d00cff8b29046c06513
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-02 23:12:41 +0530

    allow only autoscaling

--- module/kms/main.tf
+++ module/kms/main.tf
@@ -8,11 +8,47 @@ resource "aws_kms_key" "kms_key" {
   key_usage   = "ENCRYPT_DECRYPT"
   policy = jsonencode({
     "Version": "2012-10-17",
+    "Id": "key-consolepolicy-3",
     "Statement": [
       {
+        "Sid": "Enable IAM User Permissions",
         "Effect": "Allow",
         "Principal": {
-          "Service": "autoscaling.amazonaws.com"
+          "AWS": "arn:aws:iam::041445559784:root"
+        },
+        "Action": "kms:*",
+        "Resource": "*"
+      },
+      {
+        "Sid": "Allow access for Key Administrators",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "arn:aws:iam::041445559784:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
+        },
+        "Action": [
+          "kms:Create*",
+          "kms:Describe*",
+          "kms:Enable*",
+          "kms:List*",
+          "kms:Put*",
+          "kms:Update*",
+          "kms:Revoke*",
+          "kms:Disable*",
+          "kms:Get*",
+          "kms:Delete*",
+          "kms:TagResource",
+          "kms:UntagResource",
+          "kms:ScheduleKeyDeletion",
+          "kms:CancelKeyDeletion",
+          "kms:RotateKeyOnDemand"
+        ],
+        "Resource": "*"
+      },
+      {
+        "Sid": "Allow use of the key",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "arn:aws:iam::041445559784:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
         },
         "Action": [
           "kms:Encrypt",
@@ -22,9 +58,29 @@ resource "aws_kms_key" "kms_key" {
           "kms:DescribeKey"
         ],
         "Resource": "*"
+      },
+      {
+        "Sid": "Allow attachment of persistent resources",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "arn:aws:iam::041445559784:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
+        },
+        "Action": [
+          "kms:CreateGrant",
+          "kms:ListGrants",
+          "kms:RevokeGrant"
+        ],
+        "Resource": "*",
+        "Condition": {
+          "Bool": {
+            "kms:GrantIsForAWSResource": "true"
+          }
+        }
       }
     ]
   })
 }
+
+
 
 
