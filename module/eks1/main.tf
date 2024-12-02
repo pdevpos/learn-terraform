98b36aa73915a7499ae0ba898bcc82ec731e7668
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-02 11:42:36 +0530

    required kms key id

--- module/eks1/main.tf
+++ module/eks1/main.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   name     = "${var.env}-${var.component}-cluster"
   role_arn = aws_iam_role.eks_iam_role.arn
  vpc_config {
-   subnet_ids = [var.eks_subnets.id]
+   subnet_ids = [var.eks_subnets]
    security_group_ids = [aws_security_group.security.id]
  }
   encryption_config {
