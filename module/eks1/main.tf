0ca4acb2fe3f9d0de1f89a3727a443db7375e6ca
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-03 22:37:54 +0530

    to provide name for instance in launch time

--- module/eks1/main.tf
+++ module/eks1/main.tf
@@ -50,7 +50,7 @@ resource "aws_eks_node_group" "node_group" {
   }
   launch_template {
     version = "$Latest"
-    name = "${var.component}-${var.env}-launch-tmplt"
+    name = aws_launch_template.launch_template.name
   }
   tags = {
     Name = "${var.env}-${var.component}-eks-node"
