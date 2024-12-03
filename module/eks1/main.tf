4c50e3d2e1572e8fe5dcf6db19b33042922c451b
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-03 22:18:19 +0530

    create a luanch template * Add launch template to nodegroup

--- module/eks1/main.tf
+++ module/eks1/main.tf
@@ -7,13 +7,13 @@ resource "aws_eks_cluster" "eks_cluster" {
    subnet_ids = var.eks_subnets
    security_group_ids = [aws_security_group.security.id]
  }
-#   encryption_config {
-#     resources = ["secrets"]
-#     provider {
-#       key_arn = "arn:aws:kms:us-east-1:041445559784:key/fdba9587-529f-456f-92c9-a9650d9accf3"
-#
-#     }
-#   }
+  encryption_config {
+    resources = ["secrets"]
+    provider {
+      key_arn = "arn:aws:kms:us-east-1:041445559784:key/fdba9587-529f-456f-92c9-a9650d9accf3"
+
+    }
+  }
 
 tags = {
   Name = "${var.env}-${var.component}-cluster"
@@ -48,9 +48,9 @@ resource "aws_eks_node_group" "node_group" {
     max_size     = 2
     min_size     = 1
   }
-#   launch_template {
-#     version = "$Latest"
-#   }
+  launch_template {
+    version = "$Latest"
+  }
   tags = {
     Name = "${var.env}-${var.component}-eks-node"
   }
@@ -105,17 +105,17 @@ resource "aws_security_group" "security" {
     Name = "sg-${var.component}-eks"
   }
 }
-# resource "aws_launch_template" "launch_template" {
-#   block_device_mappings {
-#     device_name = "/dev/sda1"
-#     ebs {
-#       volume_size           = 20
-#       volume_type           = "gp3"
-#       delete_on_termination = true
-#       encrypted             = true
-#     }
-#   }
-# }
+resource "aws_launch_template" "launch_template" {
+  block_device_mappings {
+    device_name = "/dev/sda1"
+    ebs {
+      volume_size           = 20
+      volume_type           = "gp3"
+      delete_on_termination = true
+      encrypted             = true
+    }
+  }
+}
 # ASG is trying to use a KMS service, here there is no permission to access KMS service by ASG.
 #KMS service need to add to ASG config
 #KMS is mandatory
