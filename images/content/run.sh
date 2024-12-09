cad28f57ded554cb38a21be098d8fa12251d9376
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-09 13:50:32 +0530

    provide a name for node group

---
+++ images/content/run.sh
@@ -0,0 +1,6 @@
+mkdir -p /usr/share/nginx/html
+#while true ; do
+#  echo "<h1>Hello - $(date)</h1>" >>/usr/share/nginx/html/index.html
+#  sleep 1
+#done
+echo Hello world >/usr/share/nginx/html/index.html
