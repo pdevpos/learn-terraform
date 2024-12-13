026a51e46136953bac0226e428f8e11077985ee0
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-14 00:27:38 +0530

    * get vault secrets from vault * inject variables to initcontainer * share data from init container to container

--- module/app/main.tf
+++ module/app/main.tf
@@ -175,7 +175,51 @@ resource "aws_lb_listener" "listener" {
 //
 
 
+resource "aws_lb_listener" "frontend-http" {
+  count             = var.lb_req && var.lb_internet_type == "public" ? 1 : 0
+  load_balancer_arn = aws_lb.lb[0].arn
+  port              = var.app_port
+  protocol          = "HTTP"
 
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+
+}
+
+resource "aws_lb_listener" "frontend-https" {
+  count             = var.lb_req && var.lb_internet_type == "public" ? 1 : 0
+  load_balancer_arn = aws_lb.lb[0].arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+#   certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target[0].arn
+  }
+
+}
+
+resource "aws_lb_listener" "backend" {
+  count             = var.lb_req && var.lb_internet_type != "public" ? 1 : 0
+  load_balancer_arn = aws_lb.lb[0].arn
+  port              = var.app_port
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target[0].arn
+  }
+
+}
 
 
 
